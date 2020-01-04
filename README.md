c8fde13735c33aac943a2af18978b8df2ce1728a
L4by <laby@laby.me>
2020-01-04 12:09:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Weather Service
+Fetch temperature, pressure, humidity from current location with raspberry pi sense hat system and display to json string with python flask module.
+
+## Reference
+This is a test project. Do not use this project in a production service.
---
+++ main.py
@@ -0,0 +1,45 @@
+import threading
+import requests
+import time
+import subprocess
+from sense_hat import SenseHat
+from flask import Flask, jsonify, render_template, Response
+sense = SenseHat()
+sense.set_rotation(180)
+
+class myFlask(Flask):
+    def process_response(self, response):
+        response.headers['server'] = 'LabyCustomWeb/1.0'
+        return (response)
+
+app = myFlask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/v1/weather')
+def index_weather():
+    temp_c = sense.get_temperature()
+    humidity_data = sense.get_humidity() 
+    pressure_mb = sense.get_pressure() 
+    cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
+    array = cpu_temp.split(b"=")
+    array2 = array[1].split(b"'")
+
+    cpu_tempc = float(array2[0])
+    cpu_tempc = float("{0:.2f}".format(cpu_tempc))
+    cpu_tempf = float(array2[0]) * 9.0 / 5.0 + 32.0
+    cpu_tempf = float("{0:.2f}".format(cpu_tempf))
+
+    temp_calibrated_c = temp_c - ((cpu_tempc - temp_c)/5.466)
+    temp_calibrated_c = float("{0:.2f}".format(temp_calibrated_c))
+    humidity_data = float("{0:.2f}".format(humidity_data))
+    pressure_in = 0.0295301*(pressure_mb)
+    pressure_in = float("{0:.2f}".format(pressure_in))
+    pressure_mb = float("{0:.2f}".format(pressure_mb))
+
+    return jsonify(info_temperature=temp_calibrated_c, info_pressure=pressure_mb, info_humidity=humidity_data, about_curdate=int(time.time()), about_measure_location="todo with gps")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=50000, debug=True, threaded=True)
